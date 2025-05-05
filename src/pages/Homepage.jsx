3d6cdf15f149763193867b00a393ba5d1cbcd6cd
Mouwafic Badarou <badaroumouwafic@gmail.com>
2025-05-05 04:44:42 +0100

    Ajout des composants de page et de la configuration de routage pour l'application Worldwise

---
+++ src/pages/Homepage.jsx
@@ -0,0 +1,9 @@
+const Homepage = () => {
+  return (
+    <h1>
+      WorldWise
+    </h1>
+  );
+};
+
+export default Homepage;
---
+++ src/pages/PageNotFound.jsx
@@ -0,0 +1,9 @@
+const PageNotFound = () => {
+  return (
+    <h1>
+      Not Found :(
+    </h1>
+  );
+};
+
+export default PageNotFound;
