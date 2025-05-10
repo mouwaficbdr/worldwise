4699ac84b28cf1fea6ea330280aa58ccb9dc5f85
Mouwafic Badarou <badaroumouwafic@gmail.com>
2025-05-10 10:53:22 +0100

    Ajout de la navigation dans le composant AppNav avec des liens vers les villes et les pays.

--- src/components/AppNav.jsx
+++ src/components/AppNav.jsx
@@ -1,9 +1,17 @@
 import styles from './AppNav.module.css'
+import { NavLink } from 'react-router-dom';
 
 const AppNav = () => {
   return (
     <nav className={styles.nav}>
-      App Navigation
+      <ul>
+        <li>
+          <NavLink to='cities'>Cities</NavLink>
+        </li>
+        <li>
+          <NavLink to="countries">Countries</NavLink>
+        </li>
+      </ul>
     </nav>
   );
 };
