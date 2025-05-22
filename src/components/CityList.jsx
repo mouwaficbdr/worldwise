e0f47cc4ac12f048f70968ab222f86465b1392be
mouwaficbdr <badaroumouwafic@gmail.com>
2025-05-22 02:52:24 +0100

    Ajout du composant CityList pour afficher la liste des villes

---
+++ src/components/CityList.jsx
@@ -0,0 +1,11 @@
+import styles from './CityList.module.css'
+
+const CityList = () => {
+  return (
+    <ul className={styles.cityList}>
+      LIST
+    </ul>
+  );
+};
+
+export default CityList;
