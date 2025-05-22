004f2e6b9c1501b7b76021daed643438ea0d0419
mouwaficbdr <badaroumouwafic@gmail.com>
2025-05-22 02:52:11 +0100

    Ajout de la récupération des données des villes et mise à jour du composant CityList dans les routes.

--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,5 @@
+import { useState, useEffect } from "react"
+
 import { BrowserRouter, Routes, Route } from "react-router-dom"
 import Product from './pages/Product'
 import Pricing from './pages/Pricing'
@@ -5,8 +7,32 @@ import Homepage from './pages/Homepage'
 import PageNotFound from "./pages/PageNotFound"
 import AppLayout from "./pages/AppLayout"
 import Login from "./pages/Login"
+import CityList from "./components/CityList"
+
+const BASE_URL = 'http://localhost:8000';
 
 const App = () => {
+
+  const [cities, setCities] = useState({});
+  const [isLoading, setIsLoading] = useState(false);
+
+
+  useEffect(() => {
+    const fetchCities = async () => {
+      try {
+        setIsLoading(true)
+        const res = await fetch(`${BASE_URL}/cities`)
+        const data = await res.json()
+        setCities(data)
+      } catch {
+        alert("There was an error loading data...")
+      } finally {
+        setIsLoading(false)
+      }
+    }
+    fetchCities();
+  }, []);
+
   return (
     <BrowserRouter>
       <Routes>
@@ -16,7 +42,7 @@ const App = () => {
         <Route path="/login" element={<Login />} />
         <Route path="app" element={<AppLayout />}>
           <Route index element={<p>List of cities</p>} />
-          <Route path="cities" element={<p>List of cities</p>} />
+          <Route path="cities" element={<CityList/>} />
           <Route path="countries" element={<p>List of countries</p>} />
           <Route path="form" element={<p>Form</p>} />
         </Route>
