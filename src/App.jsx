e1a15744ff4a9a43607eaaa34eb361934783c97b
mouwaficbdr <badaroumouwafic@gmail.com>
2025-06-02 15:57:05 +0100

    Ajout du composant City pour gérer l'affichage des détails d'une ville dans les routes de l'application

--- src/App.jsx
+++ src/App.jsx
@@ -9,6 +9,7 @@ import AppLayout from "./pages/AppLayout"
 import Login from "./pages/Login"
 import CityList from "./components/CityList"
 import CountryList from "./components/CountryList"
+import City from "./components/City"
 
 const BASE_URL = 'http://localhost:8000';
 
@@ -38,20 +39,30 @@ const App = () => {
   return (
     <BrowserRouter>
       <Routes>
-        <Route index element={<Homepage/>} />
-        <Route path="product" element={<Product/>} />
+        <Route index element={<Homepage />} />
+        <Route path="product" element={<Product />} />
         <Route path="pricing" element={<Pricing />} />
-        <Route path="/login" element={<Login />} />
+        <Route path="login" element={<Login />} />
         <Route path="app" element={<AppLayout />}>
-          <Route index element={<p>List of cities</p>} />
-          <Route path="cities" element={<CityList cities={cities}  isLoading={isLoading}/>} />
-          <Route path="countries" element={<CountryList cities={cities} isLoading={isLoading}/>} />
+          <Route
+            index
+            element={<CityList cities={cities} isLoading={isLoading} />}
+          />
+          <Route
+            path="cities"
+            element={<CityList cities={cities} isLoading={isLoading} />}
+          />
+          <Route path="cities/:id" element={ <City />} />
+          <Route
+            path="countries"
+            element={<CountryList cities={cities} isLoading={isLoading} />}
+          />
           <Route path="form" element={<p>Form</p>} />
         </Route>
         <Route path="*" element={<PageNotFound />} />
       </Routes>
     </BrowserRouter>
-  )
+  );
 };
 
 export default App;
