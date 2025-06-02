8737d1efdeb58c35f18f5ab7ba64e04510f3eefc
mouwaficbdr <badaroumouwafic@gmail.com>
2025-06-02 08:43:38 +0100

    Ajout du composant CountryList pour afficher les informations sur les pays dans l'application

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
 import { useState, useEffect } from "react"
-
 import { BrowserRouter, Routes, Route } from "react-router-dom"
+
 import Product from './pages/Product'
 import Pricing from './pages/Pricing'
 import Homepage from './pages/Homepage'
@@ -8,6 +8,7 @@ import PageNotFound from "./pages/PageNotFound"
 import AppLayout from "./pages/AppLayout"
 import Login from "./pages/Login"
 import CityList from "./components/CityList"
+import CountryList from "./components/CountryList"
 
 const BASE_URL = 'http://localhost:8000';
 
@@ -28,7 +29,7 @@ const App = () => {
         alert("There was an error loading data...")
       } finally {
         setIsLoading(false)
-      }
+      } 
     }
     fetchCities();
   }, []);
@@ -44,7 +45,7 @@ const App = () => {
         <Route path="app" element={<AppLayout />}>
           <Route index element={<p>List of cities</p>} />
           <Route path="cities" element={<CityList cities={cities}  isLoading={isLoading}/>} />
-          <Route path="countries" element={<p>List of countries</p>} />
+          <Route path="countries" element={<CountryList cities={cities} isLoading={isLoading}/>} />
           <Route path="form" element={<p>Form</p>} />
         </Route>
         <Route path="*" element={<PageNotFound />} />
